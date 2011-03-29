717aebec50e73136b8575d686514d90068a61c51
jruberg <rubergly@gmail.com>
2011-03-29 11:58:42 -0400

    fixed transforms; points draw correctly now

--- lib/driving/display.rb
+++ lib/driving/display.rb
@@ -1,15 +1,18 @@
 module Driving
   class Display
+    attr_accessor :map
+    
     def initialize map, p
       @map = map
 
-      # the point at the world coordinates given by @camera_pos will be centered
-      # on the screen.
-      @camera_pos = map.world_max.collect {|x| x / 2}
+      # the point at the world coordinates given by (@c_x, @c_y) will
+      # be centered on the screen.
+      @c_x = map.world_max[0] / 2.0
+      @c_y = map.world_max[1] / 2.0
 
-      # @camera_zoom specifies the distance between the center of the camera and
-      # the edge of the top or bottom screen boundaries (in world coordinates).
-      @camera_zoom = 1
+      # @z_yspecifies the distance between the center of the camera and the
+      # edge of the top or bottom screen boundaries (in world coordinates).
+      @z_y = 10
 
       # store reference to processing to access processing commands.
       @p = p
@@ -17,29 +20,33 @@ module Driving
 
     def aspect_ratio
       width() / height()
-    end  
+    end
+
+    def zoom_x
+      @z_y * aspect_ratio()
+    end
+
+    def zoom_y
+      @z_y
+    end
+      
 
     def world_to_screen(x, y)
-      a = [x, y]
-
-      a[0] = x - @camera_pos[0] - aspect_ratio() * @camera_zoom
-      a[1] = @camera_pos[0] + @camera_zoom - y
-
-      return a
+      sx = (x - (@c_x - zoom_x())) * ( width() / (2 * zoom_x()))
+      sy = ((@c_y + zoom_y()) - y) * ( height() / (2 * zoom_y()))
+      [sx, sy]
     end
 
     def on_screen?(x,y)
-      sx, sy = world_to_screen x,y
-      sx > 0 and sx < width() and sy > 0 and sy < height()
+      x > @c_x - zoom_x() and x < @c_x + zoom_x() and
+      y > @c_y - zoom_y() and y < @c_y + zoom_y()
     end
 
     # draw point specified in world coordinates (only actually draws it if it's
     # on the screen).
     def point(x, y)
-      if on_screen? x,y then
-        sx,sy = world_to_screen x,y
-        @p.point sx,sy
-      end 
+      sx,sy = world_to_screen x,y
+      @p.point sx,sy
     end
 
     def width
@@ -55,9 +62,11 @@ module Driving
 
     def draw
       @map.nodes.each do |n|
-        point n.x, n.y
+        point n.x, n.y if on_screen? n.x, n.y
       end
     end
+    
   end
+  
 end
 
